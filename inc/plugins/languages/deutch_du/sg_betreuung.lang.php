7db88e5235eebe0e468084e689d7a3857b4ae360
saen91 <69750696+saen91@users.noreply.github.com>
2026-04-25 01:02:17 +0200

    Update sg_betreuung.lang.php

--- inc/plugins/languages/deutch_du/sg_betreuung.lang.php
+++ inc/plugins/languages/deutch_du/sg_betreuung.lang.php
@@ -1,2 +1,4 @@
 <?php
 $l['sg_betreuung_alert_assigned'] = "{1} betreut jetzt dein Thema „{2}“.";
+$l['sg_betreuung_alert_newreply_single'] = "Es gibt eine neue Antwort in deinem betreuten Thema „{1}“.";
+$l['sg_betreuung_alert_newreply_multiple'] = "Du hast {1} neue Antworten in deinem betreuten Thema „{2}“.";
